a4d451d5f76840fe701cb90767899bed8a00c89d
Augusto Moita <a94moita@outlook.com>
2019-07-04 08:22:49 -0300

    Update service definition

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,9 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('motta_pg');
+        $treeBuilder = new TreeBuilder('motta_pg');
+        //$rootNode = $treeBuilder->root('motta_pg');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
